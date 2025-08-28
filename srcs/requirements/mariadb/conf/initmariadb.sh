b6ab62f41cbe152c9416abb658dbee15e401a413
edelannoperso <elisadelannoy10@gmail.com>
2025-08-28 14:29:51 +0200

    mariadb password ok

--- srcs/requirements/mariadb/conf/initmariadb.sh
+++ srcs/requirements/mariadb/conf/initmariadb.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
 set -e
 
-DATADIR="/var/lib/mysql"
+DATADIR="/var/lib/mysql" 
+echo "Init mariadb without password"
+mysql_install_db --user=mysql --datadir="$DATADIR"
 
-if [ -d "$DATADIR/mysql" ] && [ -f "$DATADIR/mysql/user.frm" ]; then
-  echo "✅ MariaDB déjà initialisé, lancement normal..."
-  exec mysqld_safe
-fi
-
-
-echo "🔧 Lancement de l'initialisation MariaDB..."
-
+echo "Start mariadb without network"
 mysqld_safe --skip-networking &
 pid=$!
 
-echo "⏳ Attente de MariaDB..."
+echo "Waiting for mariadb to be ready"
 until mysqladmin ping --silent; do
   sleep 1
 done
 
-echo "✅ Connexion OK. Configuration..."
+echo "Database initialisation"
 
-# On crée / modifie root sans mot de passe (car c'est le tout début)
 mysql -u root <<-EOSQL
-  ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '${MYSQL_ROOT_PASSWORD}';
-  FLUSH PRIVILEGES;
-EOSQL
-
-# Maintenant on peut se reconnecter avec le mot de passe root
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOSQL
+  ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+  CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+  GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
   CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
   CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
   GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
   FLUSH PRIVILEGES;
 EOSQL
 
-echo "🛑 Arrêt temporaire de MariaDB..."
+echo "Stop mariadb"
 mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+wait $pid
 
-echo "🚀 Relance de MariaDB en premier plan..."
+echo "Start new mariadb with pqssword and network"
 exec mysqld_safe
