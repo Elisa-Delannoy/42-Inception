131b37e887d49170346d4b86015f3ddfd2ca5441
edelannoperso <elisadelannoy10@gmail.com>
2025-09-03 11:35:54 +0200

    worpdress ok

---
+++ srcs/requirements/wordpress/conf/initwordpress.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+echo "⏳ Attente de la base de données mariadb..."
+
+until mysqladmin ping -h "mariadb" --silent; do
+  sleep 3
+done
+
+cd /var/www/html/wordpress
+# Vérifie si WordPress est installé, pas juste la présence du wp-config.php
+# Pas besoin de télécharger WordPress, les fichiers sont déjà présents dans l'image
+
+wp config create \
+    --dbname=$MYSQL_DATABASE \
+    --dbuser=$MYSQL_USER \
+    --dbpass=$MYSQL_PASSWORD \
+    --dbhost=mariadb \
+    --allow-root
+wp core install \
+    --url=https://edelanno.42.fr \
+    --title="42-INCEPTION" \
+    --admin_user=$WP_ADMIN \
+    --admin_password=$WP_ADMIN_PASSWORD \
+    --admin_email=$WP_ADMIN_EMAIL \
+    --skip-email \
+    --allow-root
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+exec /usr/sbin/php-fpm8.2 -F
